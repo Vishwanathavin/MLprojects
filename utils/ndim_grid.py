6378dd2be8afa063720d3f11422a9d27c34dae2b
vishwa88 <vishwanathavin@gmail.com>
2017-09-25 06:49:19 +0530

    Fun projects

---
+++ utils/ndim_grid.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# for i,j in zip(range(0,10),range(0,15)):
+#     print i,j
+text=[[0 for i in range(10)] for j in range(10)]
+# print np.linspace(i,j,10)[:-1]
+# for i in range(0,2):
+#     for j in range(0,2):
+#         text[i][j] = [i,j]
+print text
+# np.linspace(i,j,10) for i,j in zip(0,10)
